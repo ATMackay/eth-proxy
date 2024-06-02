dcfa4458539ed1038d4d1d9b1c7102460b0fbbbf
ATMackay <amackay@alluvial.finance>
2024-06-02 14:41:12 +0100

    fix docker build

--- docker/build.sh
+++ docker/build.sh
@@ -4,11 +4,12 @@ set -e
 
 commit_hash=$(git rev-parse HEAD)
 commit_hash_short=$(git rev-parse --short HEAD)
+commit_timestamp=$(git show -s --format="%ci" ${commit_hash})
 
 docker build \
-       --build-arg DATE="$(git show -s --format=%ci "$commit_hash")"\
        --build-arg SERVICE=eth-proxy \
        --build-arg GIT_SHA="$commit_hash" \
+       --build-arg GIT_TIMESTAMP="$commit_timestamp" \
        -t eth-proxy:latest  \
        -t eth-proxy:"$commit_hash_short"  \
        -f Dockerfile ..
